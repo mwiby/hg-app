9114a30a641f72ea83ad6cc322a01f3444c17a7d
Markus Byremo <markuswb77@gmail.com>
2024-10-24 14:28:36 +0200

    redo api call

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,6 @@ export const fetchLightHouses = async () => {
   
     const response = await fetch(
       'https://api.openseamap.org/api/geonames?lang=en&lat=54.1&lon=13.4'
-      // 'https://wfs.geonorge.no/skwms1/wfs.kartverket_havner?service=WFS&version=2.0.0&request=GetFeature&typeNames=kartverket_havner&outputFormat=application/json'
     );
   
     if (!response.ok) {
@@ -11,8 +10,5 @@ export const fetchLightHouses = async () => {
     }
   
     const data = await response.json();
-
-    console.log(data);
-
     return data;
   };
