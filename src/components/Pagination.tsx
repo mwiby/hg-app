bc67deb5335970677f9f131cc3495df422618ce0
Markus Byremo <markuswb77@gmail.com>
2024-11-21 06:49:04 +0100

    small changes on pagination and translations

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,49 +1,30 @@
-import {PaginationProps} from "../types/dataTypes"
+import { PaginationProps } from "../types/dataTypes";
 
 const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
-    
   const handlePageChange = (newPage: number) => {
     if (newPage > 0 && newPage <= totalPages) {
       onPageChange(newPage);
     }
   };
 
-  const renderPageNumbers = () => {
-    const pages: number[] = [];
-    for (let i = 1; i <= totalPages; i++) {
-      pages.push(i);
-    }
-    return pages.map((page) => (
-      <button
-        key={page}
-        onClick={() => handlePageChange(page)}
-        className={`px-3 py-1 mx-1 rounded-lg ${
-          page === currentPage
-            ? "bg-blue-500 text-white font-bold"
-            : "bg-gray-200 text-gray-700"
-        }`}
-      >
-        {page}
-      </button>
-    ));
-  };
-
   return (
-    <div className="flex justify-center items-center mt-6 space-x-2">
+    <div className="flex justify-center items-center mt-6 space-x-4">
       <button
         onClick={() => handlePageChange(currentPage - 1)}
         disabled={currentPage === 1}
         className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg disabled:opacity-50"
       >
-        Previous
+        Forrige
       </button>
-      <div className="flex">{renderPageNumbers()}</div>
+      <span className="text-gray-700 font-medium">
+        Side {currentPage} av {totalPages}
+      </span>
       <button
         onClick={() => handlePageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
         className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg disabled:opacity-50"
       >
-        Next
+        Neste
       </button>
     </div>
   );
