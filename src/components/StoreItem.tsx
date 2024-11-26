7cf3a33e0505d1e1160e3393090229faa49c8f01
Markus Byremo <markuswb77@gmail.com>
2024-11-26 09:55:41 +0100

    impl temp literals to concatenate opening hours for all days into one string

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,6 +1,17 @@
 import { StoreItemProps } from "../types/dataTypes";
 
 const StoreItem: React.FC<StoreItemProps> = ({ store, onClick }) => {
+
+  const openingHoursText = `
+    Man: ${store.openingHours.monday || "Stengt"}, 
+    Tir: ${store.openingHours.tuesday || "Stengt"}, 
+    Ons: ${store.openingHours.wednesday || "Stengt"}, 
+    Tor: ${store.openingHours.thursday || "Stengt"}, 
+    Fre: ${store.openingHours.friday || "Stengt"}, 
+    Lør: ${store.openingHours.saturday || "Stengt"}, 
+    Søn: ${store.openingHours.sunday || "Stengt"}
+  `;
+
   return (
     <li
       className="bg-white border border-gray-200 rounded-md shadow-sm p-4 hover:shadow-md transition duration-150 ease-in-out transform hover:scale-105 cursor-pointer"
@@ -13,9 +24,9 @@ const StoreItem: React.FC<StoreItemProps> = ({ store, onClick }) => {
       />
       <h2 className="text-lg font-medium text-gray-800">{store.name}</h2>
       <p className="text-sm text-gray-500 mb-1">{store.address}</p>
-      <p className="text-sm text-gray-500 mb-1">Phone: {store.phone}</p>
-      <p className="text-sm text-gray-500 mb-1">
-        Opening hours: {store.openingHours.monday || "Closed"}
+      <p className="text-sm text-gray-500 mb-1">Telefon: {store.phone}</p>
+      <p className="text-sm text-gray-500">
+        <b>Åpningstider: </b>{openingHoursText.trim()}
       </p>
     </li>
   );
